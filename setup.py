eb150f6eafa287073862a3f199c0e7bec6e7b383
SaifAati <saifaati@gmail.com>
2022-01-12 18:31:42 -0800

    v0.1.2: Add multi band filtering.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='geoNLMF',
-    version='0.1.1',
+    version='0.1.2',
     author="Saif Aati",
     author_email="saif@caltech.edu, saifaati@gmail.com",
     description="geoNLMF",
