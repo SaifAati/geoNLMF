cd57bc07abd44f9f2054b0d2dbdd6ff30e8f4c4a
SaifAati <saifaati@gmail.com>
2022-03-10 11:50:06 -0800

    Add Example

--- geoNLMF/Test/Test_geoNLMF.py
+++ geoNLMF/Test/Test_geoNLMF.py
@@ -1,18 +1,18 @@
 import os
 from geoNLMF.geoNLMF import cgeoNLMF
 
-iRasterPath = os.path.join(os.path.dirname(__file__), "Data/Disp2.tif")
-# iRasterPath = "/home/cosicorr/0-WorkSpace/geoCosiCorr3D_TestWorksapce/RFM_Ortho_Dev/TestRFM_Refinement/20201015_033837_0e3a_1B_Analytic_DN_oRFM_4_noDEM_VS_20210921_034958_1005_1B_Analytic_DN_oRFM_4_noDEM_geoFqCorr_W64_S8.tif"
+################################# EXAMPLE 1 ############################################################################
+iRasterPath = os.path.join(os.path.dirname(__file__), "Data/Madoi/T47SMU_20201012T040719_B02_VS_T47SMU_20211017T040749_B02_geoFqCorr_W128_S8_det.tif")
 geoNLMFObj = cgeoNLMF(iRasterPath=iRasterPath,
-                      patchSize=5,
+                      patchSize=7,
                       searchSize=41,
                       h=1.5,
                       useSNR=0,
                       adaptive=0,
                       visualize=True,
-                      bandsList=[1])
+                      bandsList=[1,2],
+                      factor=0.75)
 geoNLMFObj.geoNLMF()
+del geoNLMFObj
 
 
-# import numpy as np
-# print(list(np.arange(1,10,1)))
