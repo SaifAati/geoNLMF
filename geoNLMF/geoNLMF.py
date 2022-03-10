4d4a214f145e793c5d9c5572da08e26ffcf4bd16
SaifAati <saifaati@gmail.com>
2022-03-10 11:49:06 -0800

    Add tqdm + Fix errors + Fix memory issues + add visualization

--- geoNLMF/geoNLMF.py
+++ geoNLMF/geoNLMF.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,31 +6,23 @@ from pathlib import Path
 
 import geoRoutines.georoutines as geoRT
 from ctypes import cdll
+from tqdm import tqdm
 
 
 class cgeoNLMF:
-    __iRasterInfo = None
-
-    __oArray = None
-
-    __height = None
-    __width = None
-
     __useSNR = 0
     __patchSize = 5
     __searchSize = 21
     __h = 1.5
-
-    __adaptive = 0
     __centerPoint = 0
     __weighting = 2
     __linearRegression = 0
     __minWeight = 0.1
     __minNumberWeights = 6
-    __nbBands = 1
     __snrArray_fl = np.zeros((1, 1), dtype=np.float32)
 
     __geoNLMFLibPath = os.path.join(os.path.dirname(__file__), "lib/build/src/libgeoNLMF.so")
+    libgeoNLMF = cdll.LoadLibrary(__geoNLMFLibPath)
 
     def __init__(self, iRasterPath,
                  useSNR=0,
@@ -45,8 +37,9 @@ class cgeoNLMF:
                  minNumberWeights=None,
                  bandsList=[],
                  oRasterPath="",
-                 visualize=False
-
+                 visualize=False,
+                 debug=False,
+                 factor = 0.75
                  ):
         """
 
@@ -65,17 +58,28 @@ class cgeoNLMF:
             nbBands:
             oRasterPath:
         """
+        self.debug = debug
         self.iRasterPath = iRasterPath
-        if useSNR != None:
-            self.__useSNR = useSNR
-        if patchSize != None:
-            self.__patchSize = patchSize
-        if searchSize != None:
-            self.__searchSize = 21
-        if h != None:
-            self.__h = h
-        if adaptive != None:
-            self.__adaptive = adaptive
+        self.factor = factor
+        self.searchSize = searchSize
+        if self.searchSize == None:
+            self.searchSize = self.__searchSize
+
+        self.useSNR = useSNR
+        if self.useSNR == None:
+            self.useSNR = self.__useSNR
+        self.patchSize = patchSize
+        if self.patchSize == None:
+            self.patchSize = self.__patchSize
+
+        self.h = h
+        if self.h == None:
+            self.h = self.__h
+        self.adaptive = adaptive
+
+        if self.adaptive != None:
+            self.adaptive = adaptive
+
         if centerPoint != None:
             self.__centerPoint = 0
         if weighting != None:
@@ -87,6 +91,7 @@ class cgeoNLMF:
         if minNumberWeights != None:
             self.__minNumberWeights = minNumberWeights
         self.bandsList = bandsList
+        self.oRasterPath = oRasterPath
         if oRasterPath == "":
             self.oRasterPath = os.path.join(os.path.dirname(self.iRasterPath),
                                             Path(self.iRasterPath).stem + "_geoNLMF.tif")
@@ -94,38 +99,105 @@ class cgeoNLMF:
 
     def geoNLMF(self):
 
-        self.__iRasterInfo = geoRT.RasterInfo(self.iRasterPath)
-        # TODO: noData values are not supported
-        ## read noData value then --> nan
+        self.iRasterInfo = geoRT.RasterInfo(self.iRasterPath)
+        self.height = self.iRasterInfo.rasterHeight
+        self.width = self.iRasterInfo.rasterWidth
 
-        self.__height = self.__iRasterInfo.rasterHeight
-        self.__width = self.__iRasterInfo.rasterWidth
+        if self.useSNR == 1:
+            snrArray = self.iRasterInfo.ImageAsArray(bandNumber=3)
+            self.snrArray_fl = np.array(snrArray.flatten(), dtype=np.float32)
+        else:
+            self.snrArray_fl = np.copy(self.__snrArray_fl)
 
-        if self.__useSNR == 1:
-            snrArray = self.__iRasterInfo.ImageAsArray(bandNumber=3)
-            self.__snrArray_fl = np.array(snrArray.flatten(), dtype=np.float32)
         if len(self.bandsList) == 0:
-            self.bandsList = list(np.arange(1, self.__iRasterInfo.nbBand + 1, 1))
-
+            self.bandsList = list(np.arange(1, self.iRasterInfo.nbBand + 1, 1))
 
         oArrayList = []
-        for band_ in self.bandsList:
-            # print(self.bandsList)
-            self.iRasterArray = self.__iRasterInfo.ImageAsArray(bandNumber=int(band_))
-            self.PerformNLMF()
-            oArrayList.append(self.__oArray)
-        print(self.oRasterPath)
-        print(self.__iRasterInfo.geoTrans)
-        geoRT.WriteRaster(oRasterPath=self.oRasterPath,
-                          geoTransform=self.__iRasterInfo.geoTrans,
-                          arrayList=oArrayList,
-                          epsg=self.__iRasterInfo.EPSG_Code)
-        if self.visualize:
-            self.VisualizeFiltering(-2.5, 2.5)
+        for band_ in tqdm(self.bandsList, "Perfroming geoNLMF"):
+            print("______BAND:", band_, "____________")
 
+            self.iRasterArray = self.iRasterInfo.ImageAsArray(bandNumber=int(band_))
+            self.oArray = self.PerformNLMF()
+            oArrayList.append(self.oArray)
+
+
+        if self.debug:
+            print("oRasterPath:", self.oRasterPath)
+            print("ogeoTrans:", self.iRasterInfo.geoTrans)
+
+        geoRT.WriteRaster(oRasterPath=self.oRasterPath,
+                          geoTransform=self.iRasterInfo.geoTrans,
+                          arrayList=oArrayList,
+                          epsg=self.iRasterInfo.EPSG_Code,
+                          noData=-32767)
+        if self.visualize:
+            self.VisualizeFiltering(self.iRasterInfo,geoRT.RasterInfo(self.oRasterPath))
         return
 
-    def VisualizeFiltering(self, vmin=-0.1, vmax=0.1, cmap="RdYlBu"):
+    def PerformNLMF(self):
+        """
+        
+        Returns:
+
+        """
+        # load the library
+
+        self.libgeoNLMF.InputData.argtypes = []
+        self.libgeoNLMF.InputData.argtypes = [np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.float32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.float32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.int32),
+                                              np.ctypeslib.ndpointer(dtype=np.float32),
+                                              np.ctypeslib.ndpointer(dtype=np.float32),
+                                              np.ctypeslib.ndpointer(dtype=np.float32)]
+
+        patchSize = np.array([self.patchSize], dtype=np.int32)
+        searchAreaSize = np.array([self.searchSize], dtype=np.int32)
+        # print(searchAreaSize)
+        h = np.array([self.h], dtype=np.float32)
+        weightingMethod = np.array([self.__weighting], dtype=np.int32)
+
+        minWeightValue = np.array([self.__minWeight], dtype=np.float32)
+        minNumberOfWeights = np.array([self.__minNumberWeights], dtype=np.int32)
+        linear = np.array([self.__linearRegression], dtype=np.int32)
+        adaptive = np.array([self.adaptive], dtype=np.int32)
+        SNRint = np.array([self.useSNR], dtype=np.int32)
+        CenterInclusive = np.array([self.__centerPoint], dtype=np.int32)
+        dimension = np.array([self.height, self.width], dtype=np.int32)
+        iArray_fl = np.array(self.iRasterArray.flatten(), dtype=np.float32)
+        iArray_fl = np.ma.masked_invalid(iArray_fl)
+
+        oArray_fl = np.zeros((self.width * self.height, 1), dtype=np.float32)
+        snrArray_fl = np.copy(self.snrArray_fl)
+        print(iArray_fl)
+        self.libgeoNLMF.InputData(patchSize,
+                                  searchAreaSize,
+                                  h,
+                                  weightingMethod,
+                                  minWeightValue,
+                                  minNumberOfWeights,
+                                  linear,
+                                  adaptive,
+                                  SNRint,
+                                  CenterInclusive,
+                                  dimension,
+                                  iArray_fl,
+                                  oArray_fl,
+                                  snrArray_fl)
+
+        oArray = np.asarray(oArray_fl[:, 0]).reshape((self.height, self.width))
+        # print(self.oArray)
+        self.libgeoNLMF.InputData.argtypes = []
+        return oArray
+
+    def VisualizeFiltering(self, inRasterInfo,oRasterInfo, vmin=None, vmax=None, cmap="RdYlBu", save=True):
         """
 
         Args:
@@ -136,111 +208,37 @@ class cgeoNLMF:
         Returns:
 
         """
-        from mpl_toolkits.axes_grid1 import make_axes_locatable
-        fig, axs = plt.subplots(1, 2)
 
-        im = axs[0].imshow(self.iRasterArray, cmap=cmap, vmin=vmin, vmax=vmax)
-        axs[1].imshow(self.__oArray, cmap=cmap, vmin=vmin, vmax=vmax)
-        for ax, title in zip(axs, ["E/W Disp. before\ngeoNLMF  ", "E/W Disp. after \ngeoNLMF"]):
-            ax.axis('off')
-            ax.set_title(title)
-        # divider = make_axes_locatable(axs[-1])
-        # cax = divider.append_axes("right", size="5%", pad=0.05)
-        # cbar = plt.colorbar(im, cax=cax)
-        plt.savefig(os.path.join(os.path.dirname(self.oRasterPath), Path(self.oRasterPath).stem + ".png"), dpi=300)
-        plt.show()
-        return
+        from geoRoutines.Plotting.Plotting_Routine import ColorBar_
 
-    def PerformNLMF(self):
-        """
-        
-        Returns:
+        bands = oRasterInfo.nbBand
+        for band in range(bands):
+            fig, axs = plt.subplots(1, 2, figsize=(16, 9))
+            inArray = inRasterInfo.ImageAsArray(bandNumber=band+1)
+            filteredArray = oRasterInfo.ImageAsArray(bandNumber=band+1)
 
-        """
-        # load the library
-        libCstatCorr = cdll.LoadLibrary(self.__geoNLMFLibPath)
-        libCstatCorr.InputData.argtypes = [np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.float32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.float32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.int32),
-                                           np.ctypeslib.ndpointer(dtype=np.float32),
-                                           np.ctypeslib.ndpointer(dtype=np.float32),
-                                           np.ctypeslib.ndpointer(dtype=np.float32)]
+            stat = geoRT.cgeoStat(inputArray=inArray, displayValue=False)
 
-        patchSize = np.array([self.__patchSize], dtype=np.int32)
-        searchAreaSize = np.array([self.__searchSize], dtype=np.int32)
-        h = np.array([self.__h], dtype=np.float32)
-        weightingMethod = np.array([self.__weighting], dtype=np.int32)
+            factor = self.factor
+            if vmin == None:
+                vmin = float(stat.mean) - factor * float(stat.std)
+            if vmax == None:
+                vmax = float(stat.mean) + factor * float(stat.std)
 
-        minWeightValue = np.array([self.__minWeight], dtype=np.float32)
-        minNumberOfWeights = np.array([self.__minNumberWeights], dtype=np.int32)
-        linear = np.array([self.__linearRegression], dtype=np.int32)
-        adaptive = np.array([self.__adaptive], dtype=np.int32)
-        SNRint = np.array([self.__useSNR], dtype=np.int32)
-        CenterInclusive = np.array([self.__centerPoint], dtype=np.int32)
-        dimension = np.array([self.__height, self.__width], dtype=np.int32)
-        iArray_fl = np.array(self.iRasterArray.flatten(), dtype=np.float32)
-        oArray_fl = np.zeros((self.__width * self.__height, 1), dtype=np.float32)
-        snrArray_fl = np.copy(self.__snrArray_fl)
+            im1 = axs[0].imshow(inArray, cmap=cmap, vmin=vmin, vmax=vmax)
+            im2 = axs[1].imshow(filteredArray, cmap=cmap, vmin=vmin, vmax=vmax)
+            ColorBar_(ax=axs[0], mapobj=im1, cmap=cmap, vmin=vmin, vmax=vmax, orientation="vertical")
+            ColorBar_(ax=axs[1], mapobj=im2, cmap=cmap, vmin=vmin, vmax=vmax, orientation="vertical")
+            for ax, title in zip(axs, ["Disp. before\ngeoNLMF  ", "Disp. after \ngeoNLMF"]):
+                ax.axis('off')
+                ax.set_title(title)
 
-        libCstatCorr.InputData(patchSize,
-                               searchAreaSize,
-                               h,
-                               weightingMethod,
-                               minWeightValue,
-                               minNumberOfWeights,
-                               linear,
-                               adaptive,
-                               SNRint,
-                               CenterInclusive,
-                               dimension,
-                               iArray_fl,
-                               oArray_fl,
-                               snrArray_fl)
-
-        self.__oArray = np.asarray(oArray_fl[:, 0]).reshape((self.__height, self.__width))
+            if save:
+                plt.savefig(os.path.join(os.path.dirname(self.oRasterPath),
+                                         Path(self.oRasterPath).stem + "_b" + str(band+1) + ".png"), dpi=300)
+            fig.clear()
 
         return
 
-
-def PlotProfiles():
-    ## plot profile:
-    import geoRoutines.Plotting.Plot_Profiles as pltProfileRT
-
-    profilePath = os.path.join(os.path.dirname(__file__), "Test/Data/Profile_pline.kml")
-
-    rasterPath1 = os.path.join(os.path.dirname(__file__), "Test/Data/Disp2.tif")
-    rasterPath2 = os.path.join(os.path.dirname(__file__), "Test/Data/Disp2_B1_geoNLMF.tif")
-    # profile = pltProfileRT.ExtractProfile(rasterPath=rasterPath,
-    #                                       profilePath=profilePath, bandNumber=1, offset=True, center=2700,
-    #                                       center_minus=100, center_plus=100)
-    profile1 = pltProfileRT.ExtractProfile(rasterPath=rasterPath1,
-                                           profilePath=profilePath, bandNumber=1, offset=False)
-    profile2 = pltProfileRT.ExtractProfile(rasterPath=rasterPath2,
-                                           profilePath=profilePath, bandNumber=1, offset=False)
-    labels = ["orig.", "withgeoNLMF"]
-    fig1, (ax1) = plt.subplots(1, 1)
-    for index, profileObj_ in enumerate([profile1, profile2]):
-        ax1.tick_params(direction='in', top=True, right=True, which="both", axis="both", labelsize=14)
-        ax1.plot(profileObj_.cumDistances(), profileObj_.profileValues, linewidth="3",
-                 label=labels[index])
-    ax1.grid()
-    ax1.minorticks_on()
-    ax1.set_xlabel("Distance along profile [m]", fontsize=14)
-    ax1.set_ylabel("Displacement [m]", fontsize=14)
-    # ax1.set_ylim(-2.5, 1)
-    # ax1.set_xlim(self.cumDistances()[0], self.cumDistances()[-1])
-    plt.legend()
-    plt.show()
-    profile1.PlotProfile()
-
-    return
 
 
